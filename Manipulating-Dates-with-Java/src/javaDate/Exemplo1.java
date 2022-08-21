4b82a7b993e7c714889d6ffa4fa286adcd7752cb
Kauã dos Santos Silva <kauasantosdev@gmail.com>
2022-08-21 15:33:55 -0300

    Add Exemplo1.java

---
+++ Manipulating-Dates-with-Java/src/javaDate/Exemplo1.java
@@ -0,0 +1,13 @@
+package javaDate;
+
+import java.util.Date;
+
+public class Exemplo1 {
+    public static void main(String[] args) {
+        Date dataAtual = new Date();
+        
+        System.out.println(dataAtual);
+        
+        // Output: Sun Aug 21 15:30:26 BRT 2022
+    }
+}
