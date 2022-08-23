2fe8659bb8fe0f76ec4b44e47e10f8cd25b9b979
Kauã dos Santos Silva <kauasantosdev@gmail.com>
2022-08-23 19:43:52 -0300

    Add image for test

--- Manipulando-Datas-com-Java/src/javaCalendar/Exemplo1.java
+++ Manipulando-Datas-com-Java/src/javaCalendar/Exemplo1.java
@@ -7,7 +7,9 @@ package javaCalendar;
 import java.util.Calendar;
 
 public class Exemplo1 {
+    
     public static void main(String[] args) {
+
         // Coleta o instante no momento de execução
         Calendar dataAtual = Calendar.getInstance();
         
