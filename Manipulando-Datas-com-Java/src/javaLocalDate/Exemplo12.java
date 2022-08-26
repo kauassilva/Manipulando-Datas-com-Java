45da8ec517319812f499342dad4aae1c55ebffa3
Kauã dos Santos Silva <kauasantosdev@gmail.com>
2022-08-26 16:23:00 -0300

    Update Exemplo12.java

--- Manipulando-Datas-com-Java/src/javaLocalDate/Exemplo12.java
+++ Manipulando-Datas-com-Java/src/javaLocalDate/Exemplo12.java
@@ -11,8 +11,10 @@ public class Exemplo12 {
     public static void main(String[] args) {
         LocalDate hoje = LocalDate.now();
         LocalDate ontem = hoje.minusDays(1);
+        LocalDate amanha = hoje.plusDays(1);
         
-        System.out.println(hoje); // 2022-08-26
         System.out.println(ontem); // 2022-08-25
+        System.out.println(hoje); // 2022-08-26
+        System.out.println(amanha); // 2022-08-27
     }
 }
