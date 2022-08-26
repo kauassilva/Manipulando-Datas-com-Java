c6e5f678655b41aef33ddc6f050ebf27bc47b008
Kauã dos Santos Silva <kauasantosdev@gmail.com>
2022-08-26 16:41:39 -0300

    Update Exemplo16.java

--- Manipulando-Datas-com-Java/src/javaLocalDateTime/Exemplo16.java
+++ Manipulando-Datas-com-Java/src/javaLocalDateTime/Exemplo16.java
@@ -11,12 +11,12 @@ public class Exemplo16 {
     public static void main(String[] args) {
         LocalDateTime dataAtual = LocalDateTime.now();
         
-        System.out.println(dataAtual);
-        // 2022-08-26T15:25:17.356753100
+        System.out.println(dataAtual); // 2022-08-26T16:40:31.571950100
         
         LocalDateTime futuro = dataAtual.plusHours(1).plusDays(2).plusSeconds(12);
-        
-        System.out.println(futuro);
-        // 2022-08-28T16:25:29.356753100
+        LocalDateTime passado = dataAtual.minusHours(1).minusDays(2).minusSeconds(12);
+                
+        System.out.println(passado); // 2022-08-24T15:40:19.571950100
+        System.out.println(futuro); // 2022-08-28T17:40:43.571950100
     }
 }
